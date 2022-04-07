9d84ddd5d8b5cf5b2bfe52847b1b3c72f3ce1bb3
vnaazleen <shaikvaseemnaazleen@gmail.com>
2022-04-07 14:26:28 +0530

    Deploying

--- server.js
+++ server.js
@@ -62,6 +62,14 @@ app.use(taskRouter)
 const authRouter = require('./routes/auth')
 app.use(authRouter)
 
+if(process.env.NODE_ENV == "production") {
+    app.use(express.static("client/build"))
+    const path = require("path")
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 
 // ---------------- ROUTES ----------------
 const PORT = 3500 || process.env.PORT
